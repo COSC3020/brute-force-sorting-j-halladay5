80210fe70a90bb98b5855c585206e3f9267ac8b3
j-halladay5 <156850724+j-halladay5@users.noreply.github.com>
2024-04-09 12:01:18 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,55 @@
-function permutationSort(a) {
-    return 0;
+function permutationSort(list) {
+    var l = 0;
+    var r = list.length - 1;
+    
+    return permute(list, l, r); 
 }
+
+function permute(list, l, r) {
+    if (list.length <= l + 1)
+    {
+        return 1;
+    }
+    var count = 0;
+
+    for (var i = l; i <= r; i++) {
+        if (list[i] != list[l]) {
+            swap(list, l, i);
+        }
+
+        count += permute(list, l + 1, r);
+
+        if (checkSorted(list)) 
+        {
+            return count;
+        }
+
+        if (list[l] != list[i]) {
+            swap(list, l, i);
+        }
+    }
+
+    return count;
+}
+
+function checkSorted(list) {
+    for (let i = 0; i < list.length - 1; i++) {
+        if (list[i] > list[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+function swap(arr, a, b) {
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+    return arr;
+}
+
+var test = [4,3,2,1];
+console.log(bruteForce(test));
+
+
